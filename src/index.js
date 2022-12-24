0c1c1c8fc5de6a01c983b101be13631569d95517
Rikin <mogofa8686@galotv.com>
2022-12-24 22:04:27 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./components/App";
+
+const root = ReactDOM.createRoot(document.getElementById("root")); // createRoot(container!) if you use TypeScript
+root.render(<App />);
